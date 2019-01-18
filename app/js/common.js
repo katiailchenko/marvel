6d2d8cad747fc91b717657d473257273cb5fe529
Katia Ilchenko <k17ilch@gmail.com>
2019-01-18 13:42:27 +0200

    added preloader

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,31 @@
+var
+  images = document.images,
+  images_total_count = images.length,
+  images_loaded_count = 0,
+  preloader = document.getElementById('page-preloader'),
+  perc_display = document.getElementById('load_perc');
+
+for(var i = 0; i < images_total_count; i++) {
+  image_clone = new Image();
+  image_clone.onload = image_loaded;
+  image_clone.onerror = image_loaded;
+  image_clone.src = images[i].src;
+}
+
+function image_loaded() {
+  images_loaded_count++;
+  perc_display.innerHTML = (((100 / images_total_count) * images_loaded_count) << 0) + '%';
+
+  if(images_loaded_count >= images_total_count) {
+    setTimeout(function() {
+      if(!preloader.classList.contains('done')) {
+        preloader.classList.add('done');
+      }
+    }, 2000);
+  }
+}
+
+
 $(document).ready(function() {
 
 $('.banner-sl').slick({
@@ -5,7 +33,7 @@ $('.banner-sl').slick({
  	arrows: false,
  	appendDots: $('.banner__toggle'),
  	autoplay: true,
-  	autoplaySpeed: 3000
+  	autoplaySpeed: 4000
  });
 
 $('[data-jarallax]').jarallax({
