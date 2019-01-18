96e02546c716cc167cf5325bca66f3dfc7b2bb43
Katia Ilchenko <k17ilch@gmail.com>
2019-01-18 14:05:46 +0200

    debug

--- app/js/common.js
+++ app/js/common.js
@@ -1,16 +1,17 @@
-var
-  images = document.images,
-  images_total_count = images.length,
-  images_loaded_count = 0,
-  preloader = document.getElementById('page-preloader'),
-  perc_display = document.getElementById('load_perc');
+'use strict';
+const images = document.images;
+const images_total_count = images.length;
+const preloader = document.getElementById('page-preloader');
+const perc_display = document.getElementById('load_perc');
 
-for(var i = 0; i < images_total_count; i++) {
-  image_clone = new Image();
+for(let i = 0; i < images_total_count; i++) {
+  let image_clone = new Image();
   image_clone.onload = image_loaded;
   image_clone.onerror = image_loaded;
   image_clone.src = images[i].src;
 }
+
+let images_loaded_count = 0;
 
 function image_loaded() {
   images_loaded_count++;
